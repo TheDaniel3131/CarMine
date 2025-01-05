2f22b344d49e038933807b6ce221f11a3c06da93
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2025-01-05 19:19:11 +0800

    Updated next.config.mjs.

--- carmine-app/next.config.mjs
+++ carmine-app/next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
     images: {
         domains: [
-            'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-            'auto.dev',
-            'www.motorcarmarkdown.com',
-            'vehicle-photos-published.vauto.com',
-            'pictures.dealer.com',
-            'cdn.max.auto',
+        "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        "auto.dev",
+        "www.motorcarmarkdown.com",
+        "vehicle-photos-published.vauto.com",
+        "pictures.dealer.com",
+        "cdn.max.auto",
         ],
     },
 };
