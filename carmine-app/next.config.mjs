e44142238cf94a782a21b03767bb8792dfcf0a60
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2025-01-13 13:10:38 +0800

    Comment carmine-listings.

--- carmine-app/next.config.mjs
+++ carmine-app/next.config.mjs
@@ -53,18 +53,18 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
-      {
-        protocol: "http",
-        hostname: "carmine-listing",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "carmine-listing",
-        port: "",
-        pathname: "/**",
-      },
+      // {
+      //   protocol: "http",
+      //   hostname: "carmine-listing",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "carmine-listing",
+      //   port: "",
+      //   pathname: "/**",
+      // },
       {
         protocol: "https",
         hostname: "cdn.max.auto",
