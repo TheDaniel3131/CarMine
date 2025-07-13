e87e13f65dd0b90c65dbde6b4173615a451ab7ee
zephrous <danielpoh3131@gmail.com>
2025-07-14 02:15:12 +0800

    Updated the marketplace page.tsx from app dir.

--- carmine-app/src/app/marketplace/page.tsx
+++ carmine-app/src/app/marketplace/page.tsx
@@ -1,12 +1,12 @@
-"use client"
-
-import MarketplacePage from '@/components/marketplace/Marketplace'
-import React from 'react'
+import React, { Suspense } from "react";
+import MarketplacePage from "@/components/marketplace/Marketplace"; // renamed for clarity
 
 export default function Buy() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <MarketplacePage />
-        </div>
-    )
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Suspense fallback={<div></div>}>
+        <MarketplacePage />
+      </Suspense>
+    </div>
+  );
 }
