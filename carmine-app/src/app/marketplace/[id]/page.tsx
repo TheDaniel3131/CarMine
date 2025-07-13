ade0cc149ecb70ce3b3fe5dbdfcfae5723ae2606
zephrous <danielpoh3131@gmail.com>
2025-07-14 01:59:59 +0800

    Updated marketplace id page.

--- carmine-app/src/app/marketplace/[id]/page.tsx
+++ carmine-app/src/app/marketplace/[id]/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import CarDetailsPage from "@/components/marketplace/[id]/CarDetails";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default function CarDetails({ params }: Props) {
+  const [resolvedParams, setResolvedParams] = useState<{ id: string } | null>(
+    null
+  );
+
+  useEffect(() => {
+    params.then(setResolvedParams);
+  }, [params]);
+
+  if (!resolvedParams) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
-      <CarDetailsPage params={params} />
+      <CarDetailsPage />
     </div>
   );
 }
